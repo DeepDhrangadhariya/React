0b1ee63015442fe68e83f707dd5b58641a1c6d7a
DeepDhrangadhariya <deepgajjar008@gmail.com>
2024-05-08 00:11:49 +0530

    bootstrap_integration

---
+++ bootstrap_integration/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Navbar from './Components/Navbar';
+import Section_1 from './Components/Section_1';
+import Section_2 from './Components/Section_2';
+import Section_3 from './Components/Section_3';
+
+function App() {
+  return (
+    <>
+
+      <Navbar/>
+      <Header/>
+      <Section_1/>
+      <Section_2/>
+      <Section_3/>
+      <Footer/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ bootstrap_integration/src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="py-5 bg-black">
+            <div className="container px-5"><p className="m-0 text-center text-white small">Copyright &copy; Your Website 2023</p></div>
+        </footer>
+  )
+}
