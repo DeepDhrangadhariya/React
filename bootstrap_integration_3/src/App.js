8e0d18fdfec4aa762b6767750856dbcfb2c7c0fa
DeepDhrangadhariya <deepgajjar008@gmail.com>
2024-05-08 00:13:00 +0530

    bootstrap_integration_3

---
+++ bootstrap_integration_3/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Aside from './components/Aside';
+import Section_1 from './components/Section_1';
+import Section_2 from './components/Section_2';
+import Section_3 from './components/Section_3';
+import Section_4 from './components/Section_4';
+import Footer from './components/Footer';
+import Feedback from './components/Feedback';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Header/>
+      <Aside/>
+      <Section_1/>
+      <Section_2/>
+      <Section_2/>
+      <Section_3/>
+      <Section_4/>
+      <Footer/>
+      <Feedback/>
+    </>
+  );
+}
+
+export default App;
